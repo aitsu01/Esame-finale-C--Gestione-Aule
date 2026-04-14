0114d9f374aca54b0adc2ad0d02aa3747bd1b2a4
Gianfranco Cito <citovich1975@gmail.com>
2026-04-14 10:04:51 +0200

    first commit for the C# Project

---
+++ Data/JsonDataStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esame_Finale_C__gestione_prenotazione_aule.Data
+{
+    internal class JsonDataStore
+    {
+    }
+}
